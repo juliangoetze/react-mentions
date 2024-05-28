f787c381158710a3b442d1489fd10b94cdf48f4c
JULO01 <90795588+JULO01@users.noreply.github.com>
2024-05-28 12:06:25 +0200

    feat: adapting react-mentions to tiq

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as MentionsInput } from "./MentionsInput";
 export { default as Mention } from "./Mention";
+export { default as Highlighter } from "./Highlighter";
+export { getMentions } from "./utils";
